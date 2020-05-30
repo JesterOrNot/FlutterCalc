64e83b61f6472d6d539b96713068e1eab4d353dc
Sean Hellum <seanhellum45@gmail.com>
2020-05-30 20:00:10 +0000

    use plusminusvariant

--- calc/lib/keypad.dart
+++ calc/lib/keypad.dart
@@ -11,7 +11,7 @@ class KeyPad extends StatelessWidget {
         SizedBox(
           width: 2,
         ),
-        CalculatorButton(icon: Icon(MdiIcons.plusMinus)),
+        CalculatorButton(icon: Icon(MdiIcons.plusMinusVariant)),
         SizedBox(
           width: 2,
         ),
